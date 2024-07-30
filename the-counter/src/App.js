dae9b9dea6e4f57fc33e811a05a29409023ab82c
KanchanaMadu <it20249366@my.sliit.lk>
2024-07-30 12:07:58 +0530

    Remove un-relataed files

--- the-counter/src/App.js
+++ the-counter/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import Home from  './Pages/Home'
 import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 
